e77b47cde032663a698b10d9e3ad792401435314
Bucky <buckyroberts@gmail.com>
2016-05-28 06:14:10 -0400

    Working on template

--- config/urls.py
+++ config/urls.py
@@ -7,10 +7,10 @@ urlpatterns = [
 
     # API
     url(r'^v1/api-tokens/(?P<token>[\S]+)/$', api_token.view_single_token),
-    url(r'^v1/majors/$', major.MajorView.as_view()),
-    url(r'^v1/videos/(?P<course_id>[\S]+)/$', video.VideoView.as_view()),
+    url(r'^v1/majors/$', major.MajorView.as_view(), name='majors'),
+    url(r'^v1/videos/(?P<course_id>[\S]+)/$', video.VideoView.as_view(), name='videos'),
     url(r'^v1/videos/$', video.VideoView.as_view()),
-    url(r'^v1/(?P<major_slug>[\S]+)/$', course.CourseView.as_view()),
+    url(r'^v1/(?P<major_slug>[\S]+)/$', course.CourseView.as_view(), name='courses'),
 
     # Website
     url(r'^', include('website.urls')),
